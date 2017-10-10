0f24bd113e520dd5a14d79d824325260d2f63c0f
Jonas Töremar <jonas.toremar@chasacademy.se>
2017-10-10 15:19:09 +0200

    fixed some strange choises I made regarding functions

--- js/script.js
+++ js/script.js
@@ -5,38 +5,41 @@ var selectedWord; // Ett av orden valt av en slumpgenerator
 var letterBoxes; //Rutorna där bokstäverna ska stå   // = selectedWord.length
 var hangmanImg; //Bild som kommer vid fel svar
 var hangmanImgNr; // Vilken av bilderna som kommer upp beroende på hur många fel du gjort
-var msgElem = ['Du har förlorat.', 'Du har vunnit!']; // Ger meddelande när spelet är över
+var msgElem; // Ger meddelande när spelet är över
 var startGameBtn; // Knappen du startar spelet med
 var letterButtons; // Knapparna för bokstäverna
 var startTime; // Mäter tiden
 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
 // Initiering av globala variabler samt koppling av funktioner till knapparna.
-function init() {} // End init
+function init() {
+	document.querySelector('#startGameBtn').addEventListener('click', gameStart());
+	var letterButtons = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
+
+} // End init
 
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-var gameStart = function(){
+function gameStart(){
 		return randomWord() + numberOfTiles();
 }
 
-document.getQuerySelector('#startGameBtn').addEventListener('click' gameStart);
 
 // Funktion som slumpar fram ett ord
-var randomWord = function() {
+function randomWord() {
 	selectedWord = wordList[Math.floor(Math.random() * wordList.length)];
 	return selectedWord;
 };
  
 // Funktionen som tar fram bokstävernas rutor, antal beror på vilket ord
-var numberOfTiles = function() {
+function numberOfTiles() {
 	letterBoxes = selectedWord.length;
 	return letterBoxes;
 }
 
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
-var btnClick = function() {
+function btnClick() {
 	document.getQuerySelector('.btn')//???
 };
 
