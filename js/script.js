ec7d0414889cc13b3127b70eb53c153bd287df4f
Jonas Töremar <jonas.toremar@chasacademy.se>
2017-10-10 12:01:52 +0200

    added some crappy pseudo code comments

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var selectedWord; // Ett av orden valt av en slumpgenerator
 var letterBoxes; //Rutorna där bokstäverna ska stå   // = selectedWord.length
 var hangmanImg; //Bild som kommer vid fel svar
 var hangmanImgNr; // Vilken av bilderna som kommer upp beroende på hur många fel du gjort
-var msgElem = "Du har förlorat."; // Ger meddelande när spelet är över
+var msgElem = ['Du har förlorat.', 'Du har vunnit!']; // Ger meddelande när spelet är över
 var startGameBtn; // Knappen du startar spelet med
 var letterButtons; // Knapparna för bokstäverna
 var startTime; // Mäter tiden
@@ -17,6 +17,10 @@ function init() {} // End init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
+var gameStart(){
+	randomWord();
+	letterBoxes();
+}
 
 // Funktion som slumpar fram ett ord
 var randomWord = function() {
@@ -27,10 +31,14 @@ var randomWord = function() {
 // Funktionen som tar fram bokstävernas rutor, antal beror på vilket ord
 var numberOfTiles = function() {
 	letterBoxes = selectedWord.length;
+	return letterBoxes;
 }
 
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
 
 // Funktionen ropas vid vinst eller förlust, gör olika saker beroende av det
+//OM vinst så kalla på msgElem[0] OM förlust kalla på msgElem[1]
+
 
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+//OM bokstaven är vald SÅ add innerHTML diabled på slutet av elementet
