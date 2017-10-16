f8b09a907d766f20718022f60121b5f7840399ce
Jonas Töremar <jonas.toremar@chasacademy.se>
2017-10-16 11:48:32 +0200

    letters now appear in their correct position in the input boxes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
 // Globala variabler
 
-var wordList = ['Blues', 'Rangers', 'Hurricanes', 'Pengiuns', 'Kings', 'Bruins', 'Jets']; // Lista med spelets alla ord
+// var wordList = ['BLUES', 'RANGERS', 'HURRICANES', 'PENGUINS', 'KINGS', 'BRUINS', 'JETS']; // Lista med spelets alla ord
+var wordList = ['CHAS ACADEMY']; // Lista med spelets alla ord
 var selectedWord; // Ett av orden valt av en slumpgenerator
-var letterBoxes; //Rutorna där bokstäverna ska stå   // = selectedWord.length
+var letterBoxes; //Rutorna där bokstäverna ska stå   //
 var hangmanImg; //Bild som kommer vid fel svar
 var hangmanImgNr; // Vilken av bilderna som kommer upp beroende på hur många fel du gjort
 var msgElem; // Ger meddelande när spelet är över
 var startGameBtn; // Knappen du startar spelet med
-var letterButtons; // Knapparna för bokstäverna
+var letterButtons = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
 var startTime; // Mäter tiden
+//var letter;
 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
 // Initiering av globala variabler samt koppling av funktioner till knapparna.
 function init() {
-	var letterButtons = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
-	var wordList = ['Blues', 'Rangers', 'Hurricanes', 'Pengiuns', 'Kings', 'Bruins', 'Jets']; // Lista med spelets alla ord
+	//var letterButtons = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
+	//var wordList = ['Blues', 'Rangers', 'Hurricanes', 'Pengiuns', 'Kings', 'Bruins', 'Jets']; // Lista med spelets alla ord
 	
-
 	startGameBtn;
 	letterButtons;
+
 	
 	document.querySelector('#startGameBtn').onclick = gameStart;
-	document.querySelector('#letterButtons').onclick = btnClick;
+	//document.querySelector('#letterButtons').onclick = btnClick;
+
+
+
+
 
 } // End init
 
@@ -33,38 +39,114 @@ function gameStart(){
 	randomWord();
 	numberOfTiles();
 
+
 	document.getElementById('hangman').src = "images/h0.png";
 
+	// console.log(randomWord());	//temporär kontroll
+	// console.log(numberOfTiles());
 }
-
 
 
 // Funktion som slumpar fram ett ord
 function randomWord() {
+
+	letterButtons = document.getElementsByTagName("button");
+          for (let i = 0; i < letterButtons.length; i++){    
+         	 letterButtons[i].onclick = btnClick;
+    }
+
 	selectedWord = wordList[Math.floor(Math.random() * wordList.length)];
+	selectedWord = selectedWord.replace(" ", "-").split("");
+
+	
 	return selectedWord;
 };
+
  
 // Funktionen som tar fram bokstävernas rutor, antal beror på vilket ord
 function numberOfTiles() {
-	
-	var tile = [];
+	letterBoxes = document.querySelector('#letterBoxes > ul'); //
+	let letterBox;
 
-	for (var i = 0; i < selectedWord.length; i++){
-		tile[i] = '<input type="text" value="&nbsp;" />'; //för varje bokstav i selectedWord ska tile få ett html-element.
+	for (let i = 0; i < selectedWord.length; i++){
+		letterBox = document.createElement('li');
+		letterBox.innerHTML = '<input type="text" value="&nbsp;" id="letter"/>';
+		letterBoxes.appendChild(letterBox);
 	}
-	document.querySelector('#letterBoxes').innerHTML = tile;
 
 	return letterBoxes;
 }
 
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
-function btnClick() {
+function btnClick () {
+	var letterValue = this.value;
 
-	console.log('button click successful')
-	return;
+	console.log('The letter is: ', letterValue);
+
+	var letterIndices = []; // the indices for the letter
+	var letterIndex = selectedWord.indexOf(letterValue);			
+
+	while(letterIndex !== -1) {
+		letterIndices.push(letterIndex);
+		letterIndex = selectedWord.indexOf(letterValue, letterIndex + 1);
+	}
+
+	if (letterIndices.length > 0) {
+		let listEls = letterBoxes.children;
+
+		console.log(letterValue + '  finns på index ', letterIndices, 'i ord-arrayen: ',  selectedWord);
+		console.log(listEls);
+
+
+		//skriv en loop så att letterIndices matchar index-platserna i selectedWord/listEls!
+		for (let i = 0; i < selectedWord.length; i++){
+			
+			if(selectedWord[i] == letterValue){
+			 	listEls[i].firstChild.value = letterValue;	
+			}
+			
+		}
+
+	} else {
+		// gissat fel
+		//wrongguess++; ???
+		console.log('wrong button, hombre.')
+	}
+
+	//console.log(letterIndices)
+
+	// 	for (let i = 0; i < selectedWord.length; i++) {
+					
+	// 		var index = selectedWord.indexOf(letterValue);			
+			
+	// 		if (letterValue === selectedWord[i]){		//  selectedWord.indexOf(letterValue)
+	// 			document.querySelector('#letter').value = letterValue;
+	// 			document.querySelector('#letter').id = index;
+	// 			console.log(index);
+	// 		}
+
+	// 		//TA FRAM ARRAYPLATSEN PÅ BOKSTAVEN OCH SKJUT FRAM BOKSTAVEN SÅ MÅNGA STEG I INPUT???   	
+	// 	console.log('RIGHT BUTTON WAS CLICKED')
+
+	// 	if (selectedWord.indexOf(letterValue) < -1){
+	// 		console.log('wrong')
+	// 		console.log(index);
+	// 		//mer skit här
+	// 	}
+	// }
+
+	// return;
+
 };
 
+function correctGuess(letterIndices, guessedLetter) {
+
+};
+
+function incorrectGuess() {
+	//IF wrongguess > 6 THEN terminate process
+	// Keep track of number of guesses somehow...
+};
 
 // Funktionen ropas vid vinst eller förlust, gör olika saker beroende av det
 //OM vinst så kalla på msgElem[0] OM förlust kalla på msgElem[1] och 
